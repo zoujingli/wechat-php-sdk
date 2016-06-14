38e3ea1f4409745bcb481a8169a6a0706c311218
Anyon <zoujingli@qq.com>
2016-06-14 10:00:06 +0800

    微信SDK接入文件

---
+++ include.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 获取微信操作对象
+ * @staticvar array $wechat
+ * @param type $type    接口类型
+ * @param type $config  SDK配置(token,appid,appsecret,encodingaeskey,mch_id,partnerkey,ssl_cer,ssl_key,qrc_img)
+ * @return \className
+ */
+function &load_wechat($type = '', $config = array()) {
+    static $wechat = array();
+    if (!isset($wechat[$type])) {
+        $className = "Wechat" . ucfirst(strtolower($type));
+        if (!class_exists($className, false)) {
+            require dirname(__FILE__) . "/{$className}.php";
+        }
+        $wechat[$type] = new $className($config);
+    }
+    return $wechat[$type];
+}
