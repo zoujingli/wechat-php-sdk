19248e9dfb2938faab034ebe373bb3e2455873b1
Anyon <zoujingli@qq.com>
2016-06-15 09:10:50 +0800

    no commit message

--- include.php
+++ include.php
@@ -5,14 +5,14 @@
  * @staticvar array $wechat
  * @param type $type    接口类型
  * @param type $config  SDK配置(token,appid,appsecret,encodingaeskey,mch_id,partnerkey,ssl_cer,ssl_key,qrc_img)
- * @return \className
+ * @return WechatBasic
  */
 function &load_wechat($type = '', $config = array()) {
     static $wechat = array();
     if (!isset($wechat[$type])) {
         $className = "Wechat" . ucfirst(strtolower($type));
         if (!class_exists($className, false)) {
-            require __DIR__ . "/{$className}.php"; 
+            require __DIR__ . "/{$className}.php";
         }
         $wechat[$type] = new $className($config);
     }
