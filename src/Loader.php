efa27bacbe5e631ab33e404f5d7c64f65189eddc
邹景立 <zoujingli@qq.com>
2016-08-21 13:27:17 +0800

    微信加密解密

--- src/Loader.php
+++ src/Loader.php
@@ -1,77 +1,54 @@
 <?php
 
-namespace Wechat {
+namespace Wechat;
+
+/**
+ * 微信SDK加载器
+ * 
+ * @author Anyon <zoujingli@qq.com>
+ * @date 2016-08-21 11:06
+ */
+class Loader {
 
     /**
-     * 微信SDK加载器
-     * 
-     * @author Anyon <zoujingli@qq.com>
-     * @date 2016-08-21 11:06
+     * 表态配置参数
+     * @var type 
      */
-    class Loader {
+    static protected $config;
 
-        /**
-         * 表态配置参数
-         * @var type 
-         */
-        static protected $config;
-
-        /**
-         * 设置配置参数
-         * @param type $config
-         */
-        static public function set_config($config) {
-            self::$config;
-        }
-
-        /**
-         * 获取配置参数
-         * @return type
-         */
-        static public function get_config() {
-            return self::$config;
-        }
-
-        /**
-         * 获取微信SDK接口对象
-         * @staticvar array $wechat
-         * @param type $type 接口类型(Card|Custom|Device|Extends|Media|Menu|Oauth|Pay|Receive|Script|User)
-         * @param type $config SDK配置(token,appid,appsecret,encodingaeskey,mch_id,partnerkey,ssl_cer,ssl_key,qrc_img)
-         * @return \Wechat\WechatReceive
-         */
-        static public function & get_instance($type, $config = array()) {
-            static $wechat = array();
-            $index = md5(strtolower($type));
-            if (!isset($wechat[$index])) {
-                $className = "\\Wechat\\Wechat" . ucfirst(strtolower($type));
-                $classObj = 'Wechat' . ucfirst(strtolower($type));
-                if (class_alias($className, $classObj, TRUE)) {
-                    $wechat[$index] = new $classObj($config);
-                } else {
-                    return false;
-                }
-            }
-            return $wechat[$index];
-        }
-
+    /**
+     * 设置配置参数
+     * @param type $config
+     */
+    static public function set_config($config) {
+        self::$config;
     }
 
-}
-
-namespace {
-
-    if (!function_exists('load_wechat')) {
-
-        /**
-         * 获取微信操作对象 
-         * @staticvar array $wechat
-         * @param type $type 接口类型(Card|Custom|Device|Extends|Media|Menu|Oauth|Pay|Receive|Script|User)
-         * @param type $config SDK配置(token,appid,appsecret,encodingaeskey,mch_id,partnerkey,ssl_cer,ssl_key,qrc_img)
-         * @return \Wechat\WechatReceive
-         */
-        function &load_wechat($type = '', $config = array()) {
-            return \Wechat\Loader::get_instance($type, $config);
-        }
-
+    /**
+     * 获取配置参数
+     * @return type
+     */
+    static public function get_config() {
+        return self::$config;
     }
+
+    /**
+     * 获取微信SDK接口对象
+     * @staticvar array $wechat
+     * @param type $type 接口类型(Card|Custom|Device|Extends|Media|Menu|Oauth|Pay|Receive|Script|User)
+     * @param type $config SDK配置(token,appid,appsecret,encodingaeskey,mch_id,partnerkey,ssl_cer,ssl_key,qrc_img)
+     * @return \Wechat\WechatReceive
+     */
+    static public function & get_instance($type, $config = array()) {
+        static $wechat = array();
+        $index = md5(strtolower($type));
+        if (!isset($wechat[$index])) {
+            $className = "\\Wechat\\Wechat" . ucfirst(strtolower($type));
+            $classAlias = 'Wechat' . ucfirst(strtolower($type));
+            !class_exists($classAlias, FALSE) && class_alias($className, $classAlias);
+            $wechat[$index] = new $className($config);
+        }
+        return $wechat[$index];
+    }
+
 }
