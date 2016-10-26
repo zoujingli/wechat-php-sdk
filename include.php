ed437574e3379c193f9ad380892bbaaf3161154f
邹景立 <zoujingli@qq.com>
2016-10-26 14:51:56 +0800

    重构SDK结构与加载机制

--- include.php
+++ include.php
@@ -1,19 +1,3 @@
 <?php
 
-/**
- * 非Composer模式注册SDK加载函数
- * 
- * @author Anyon <zoujingli@qq.com>
- * @date 2016-08-21 11:26
- */
-if (!class_exists('Wechat\Loader', FALSE)) {
-    /** 
-     * 注册自动加载函数
-     */
-    spl_autoload_register(function($class) {
-        if (0 === stripos($class, 'Wechat\\')) {
-            $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-            file_exists($filename) && require($filename);
-        }
-    });
-}
+require __DIR__ . DIRECTORY_SEPARATOR . 'Wechat' . DIRECTORY_SEPARATOR . 'Loader.php';
