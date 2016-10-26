79c537ca96cbd1e344d6cd2ae44b09fdc85902ef
邹景立 <zoujingli@qq.com>
2016-10-26 16:02:02 +0800

    分离并完善门店接口

--- Wechat/WechatCard.php
+++ Wechat/WechatCard.php
@@ -28,8 +28,6 @@ class WechatCard extends Common {
     const CARD_SEND_HTML = '/card/mpnews/gethtml?';
     const CARD_BATCHGET = '/card/batchget?';
     const CARD_MODIFY_STOCK = '/card/modifystock?';
-    const CARD_LOCATION_BATCHADD = '/card/location/batchadd?';
-    const CARD_LOCATION_BATCHGET = '/card/location/batchget?';
     const CARD_GETCOLORS = '/card/getcolors?';
     const CARD_QRCODE_CREATE = '/card/qrcode/create?';
     const CARD_CODE_CONSUME = '/card/code/consume?';
@@ -328,54 +326,6 @@ class WechatCard extends Common {
             return false;
         }
         $result = Tools::httpGet(self::API_BASE_URL_PREFIX . self::CARD_GETCOLORS . "access_token={$this->access_token}");
-        if ($result) {
-            $json = json_decode($result, true);
-            if (!$json || !empty($json['errcode'])) {
-                $this->errCode = $json['errcode'];
-                $this->errMsg = $json['errmsg'];
-                return $this->checkRetry(__FUNCTION__, func_get_args());
-            }
-            return $json;
-        }
-        return false;
-    }
-
-    /**
-     * 拉取门店列表
-     * 获取在公众平台上申请创建的门店列表
-     * @param int $offset  开始拉取的偏移，默认为0从头开始
-     * @param int $count   拉取的数量，默认为0拉取全部
-     * @return boolean|array   返回数组请参看 微信卡券接口文档 的json格式
-     */
-    public function getCardLocations($offset = 0, $count = 0) {
-        if (!$this->access_token && !$this->getAccessToken()) {
-            return false;
-        }
-        $data = array('offset' => $offset, 'count' => $count);
-        $result = Tools::httpPost(self::API_BASE_URL_PREFIX . self::CARD_LOCATION_BATCHGET . "access_token={$this->access_token}", Tools::json_encode($data));
-        if ($result) {
-            $json = json_decode($result, true);
-            if (!$json || !empty($json['errcode'])) {
-                $this->errCode = $json['errcode'];
-                $this->errMsg = $json['errmsg'];
-                return $this->checkRetry(__FUNCTION__, func_get_args());
-            }
-            return $json;
-        }
-        return false;
-    }
-
-    /**
-     * 批量导入门店信息
-     * @tutorial 返回插入的门店id列表，以逗号分隔。如果有插入失败的，则为-1，请自行核查是哪个插入失败
-     * @param array $data    数组形式的json数据，由于内容较多，具体内容格式请查看 微信卡券接口文档
-     * @return boolean|string 成功返回插入的门店id列表
-     */
-    public function addCardLocations($data) {
-        if (!$this->access_token && !$this->getAccessToken()) {
-            return false;
-        }
-        $result = Tools::httpPost(self::API_BASE_URL_PREFIX . self::CARD_LOCATION_BATCHADD . "access_token={$this->access_token}", Tools::json_encode($data));
         if ($result) {
             $json = json_decode($result, true);
             if (!$json || !empty($json['errcode'])) {
