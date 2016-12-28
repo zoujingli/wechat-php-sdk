3b7041b8a18829a928084ed6f0f16c911ed3170c
hzg0403 <work@liuhai.org>
2016-12-28 11:40:33 +0800

    修正获取关注者详细信息可能出现的错误
    如用户信息某一字段存在特殊字符，这导致JSON_DECODE返回NULL，现对微信服务器请求结果进行编码替换后在进行解析。

--- Wechat/WechatUser.php
+++ Wechat/WechatUser.php
@@ -93,6 +93,7 @@ class WechatUser extends Common {
         }
         $result = Tools::httpGet(self::API_URL_PREFIX . self::USER_INFO_URL . "access_token={$this->access_token}&openid={$openid}");
         if ($result) {
+            $result = substr(str_replace('\"', '"', json_encode($result)), 1, -1);
             $json = json_decode($result, true);
             if (isset($json['errcode'])) {
                 $this->errCode = $json['errcode'];
